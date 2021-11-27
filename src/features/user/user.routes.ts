b8836856eba4d7e68b2f7f463285942c89b93a39
Julian Hecker <julianheckerdev@gmail.com>
2021-11-27 16:15:23 -0500

    add apidocs

--- src/features/user/user.routes.ts
+++ src/features/user/user.routes.ts
@@ -1,8 +1,27 @@
 import { Router } from 'express';
-import { getUserById, getUsers } from './user.controller';
+import Container from 'typedi';
+
+import { UserController } from './user.controller';
+
+const userController = Container.get(UserController);
 
 export const userRouter = Router();
 
-userRouter.get('/', getUsers);
+/**
+ * @api {get} /user Get all users
+ *
+ * @apiGroup Users
+ * @apiName getAllUsers
+ * @apiVersion 1.0.0
+ */
+userRouter.get('/', userController.getAllUsers);
 
-userRouter.get('/:id', getUserById);
+/**
+ * @api {get} /user/:id Get a user by id
+ * @apiParam {String} id User ID
+ *
+ * @apiGroup Users
+ * @apiName getUserByName
+ * @apiVersion 1.0.0
+*/
+userRouter.get('/:id', userController.getUserByName);
