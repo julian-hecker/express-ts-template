67fea617dc9ce7225a90915f02c1515a5bc9bc2f
Julian Hecker <julianheckerdev@gmail.com>
2021-11-27 16:16:58 -0500

    add express middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import compression from 'compression';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import { errorHandler, notFoundHandler } from './middlewares';
 import { router } from './routes';
@@ -8,7 +13,10 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(router);
 
